fc121dfe75d899b5294534f3ab99f16721cab05a
abhayBhosle <abhaybhosle699@gmail.com>
2023-10-29 20:58:51 +0530

    ..

---
+++ Stacks/ReverseAString.java
@@ -0,0 +1,25 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class ReverseAString {
+    public static String reverseString(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        StringBuilder reversed = new StringBuilder();
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+        }
+        return reversed.toString();
+
+    }
+
+    public static void main(String[] args) {
+        String input = "hello world";
+        String reversed = reverseString(input);
+        System.out.println("Original String: " + input);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
